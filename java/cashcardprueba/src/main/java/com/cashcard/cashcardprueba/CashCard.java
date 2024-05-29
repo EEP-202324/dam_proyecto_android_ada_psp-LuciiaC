1038f2c4a8fca652e3428a84e6c86b6b4641289f
Lucia C <lucia-copa@eep-igroup.com>
2024-05-29 12:43:29 +0200

    He cambiado localización del proyecto

---
+++ java/cashcardprueba/src/main/java/com/cashcard/cashcardprueba/CashCard.java
@@ -0,0 +1,5 @@
+package com.cashcard.cashcardprueba;
+
+record CashCard(Long id, Double amount) {
+
+}
---
+++ java/cashcardprueba/src/test/java/com/cashcard/cashcardprueba/CashcardpruebaApplicationTests.java
@@ -0,0 +1,25 @@
+package com.cashcard.cashcardprueba;
+
+import com.jayway.jsonpath.DocumentContext;
+import com.jayway.jsonpath.JsonPath;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT) // Empieza Spring Boot application y make it available para nuestro test to perform requests to do it.//
+class CashCardApplicationTests {
+    @Autowired
+    TestRestTemplate restTemplate;
+
+    @Test
+    void shouldReturnACashCardWhenDataIsSaved() {
+        ResponseEntity<String> response = restTemplate.getForEntity("/cashcards/99", String.class);
+
+        assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
+    }
+}
